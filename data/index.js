f0b1d62399f42bb00ea42edf36f32a642be9e4a7
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-03 21:04:47 -0500

    added server

---
+++ data/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const postsRouter = require("../postsRouter")
+
+const server = express()
+const port = 8000
+
+server.use(express.json())
+server.use(postsRouter)
+
+server.listen(port, ()=>{
+    console.log(`server is running on at http://localhost:${port}`)
+})
